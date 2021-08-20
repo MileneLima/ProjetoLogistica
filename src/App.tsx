e0d3199adf8d6a14707ccbe91931183cc96008f0
MileneLima <mileneaparecidalima@gmail.com>
2021-08-20 08:26:59 -0300

    Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+
 import GlobalStyle from "./styles/global";
 
-import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
+import AuthProvider from "./hooks";
 
 const App: React.FC = () => {
   return (
     <>
-    <SignUp></SignUp>
-    <GlobalStyle></GlobalStyle>
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
+
+      <GlobalStyle />
     </>
-  )
+  );
 };
 
 export default App;
